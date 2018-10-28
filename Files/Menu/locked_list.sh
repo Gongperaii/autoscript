3d9b04b5bb95cc231a38d4c882c82b3bfec0352d
gongperai3838 <44527506+gongperai3838@users.noreply.github.com>
2018-10-28 15:09:34 +0800

    Add files via upload

---
+++ Files/Menu/locked_list.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script by : _Gongperai_
+if [ -f "$FILE" ];
+then
+cd
+nano /etc/Locked_List.txt
+else
+cd
+nano /etc/Locked_List.txt
+fi
---
+++ Files/Menu/menu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Script by : _Gongperai_
+clear
+echo -e "\e[0m                                                   "
+echo -e "\e[94m    :::::::::  :::::::::   ::::::::  :::   :::    "
+echo -e "\e[94m    :+:    :+: :+:    :+: :+:    :+: :+:   :+:    "
+echo -e "\e[94m    +:+    +:+ +:+    +:+        +:+  +:+ +:+     "
+echo -e "\e[94m    +#+    +:+ +#++:++#:      +#++:    +#++:      "
+echo -e "\e[94m    +#+    +#+ +#+    +#+        +#+    +#+       "
+echo -e "\e[94m    #+#    #+# #+#    #+# #+#    #+#    #+#       "
+echo -e "\e[94m    #########  ###    ###  ########     ###       "
+echo -e "\e[93m           AutoScriptVPS by  _Gongperai_           "
+echo -e "\e[0m                                                   "
+echo -e "\e[93m             [accounts/options/server]            "
+echo -e "\e[0m                                                   "
